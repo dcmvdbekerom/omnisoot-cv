182afc4b7ee89080900669efc999758829334302
dcmvdbekerom <dcmvdbekerom@gmail.com>
2024-08-13 10:29:54 +0200

    formatting

--- examples/Reactors/PlugFlow/ethylene_pyrolysis/plot.py
+++ examples/Reactors/PlugFlow/ethylene_pyrolysis/plot.py
@@ -6,23 +6,23 @@ from matplotlib.ticker import (MultipleLocator, AutoMinorLocator);
 
 # Function to save plots to disk
 def _save_plot(name, save_pdf = True):
-    output_dir = f"{POSTPROCESS_DIR}";
+    output_dir = f'{POSTPROCESS_DIR}';
     if not os.path.exists(output_dir):
         os.makedirs(output_dir);
     plt.tight_layout(pad=0.3);
-    plt.savefig(f"{output_dir}/{name}.svg");
+    plt.savefig(f'{output_dir}/{name}.svg');
     if save_pdf:
-        plt.savefig(f"{output_dir}/{name}.pdf")
+        plt.savefig(f'{output_dir}/{name}.pdf')
         
 
 # Plot style constant
-RESULTS_DIR = "results";
-DATA_DIR = "data";
-POSTPROCESS_DIR = "post_process";
+RESULTS_DIR = 'results';
+DATA_DIR = 'data';
+POSTPROCESS_DIR = 'post_process';
 
-LINESTYLES = ["solid", "dashed", "dashdot", "dotted"];
-COLORS = ["#FF0000", "#00FF00", "#0000FF", "#00FFFF", "#fc9d03", "#FF00FF", "#000000",
-         "#ff0080", "#ff8000", "#4e5a00", "#a4805c", "#cb909a"];
+LINESTYLES = ['solid', 'dashed', 'dashdot', 'dotted'];
+COLORS = ['#ff0000', '#00ff00', '#0000ff', '#00ffff', '#fc9d03', '#ff00ff', '#000000',
+          '#ff0080', '#ff8000', '#4e5a00', '#a4805c', '#cb909a'];
 
 LINEWIDTH = 1.5;
 MARKERSIZE = 10;
@@ -35,9 +35,9 @@ AXIS_FONTSIZE = 24;
 LETTER_FONTSIZE = 36;
 
 LABELS = {
-    "Monodisperse" : "Monodisperse",
-    "Sectional" : "Sectional",
-    "Data" : "Araki et al. (2019)"
+    'Monodisperse' : 'Monodisperse',
+    'Sectional' : 'Sectional',
+    'Data' : 'Araki et al. (2019)'
 }
 
 def plot(
@@ -52,17 +52,17 @@ def plot(
     # Reading the results of simulations for a given PAH growth model with
     # Monodisperse and Sectional population balance models
     for particle_dynamics_model_type in particle_dynamics_model_types:
-        filepath = f"{RESULTS_DIR}/{particle_dynamics_model_type}/{PAH_growth_model_type}/sim_results.csv";
+        filepath = f'{RESULTS_DIR}/{particle_dynamics_model_type}/{PAH_growth_model_type}/sim_results.csv';
         results_df[particle_dynamics_model_type] = pd.read_csv(filepath);
         
     # Experimental Data
-    exp_df = pd.read_csv(f"{DATA_DIR}/experimental_data.csv");
+    exp_df = pd.read_csv(f'{DATA_DIR}/experimental_data.csv');
     
     param_dict = {
-        "soot_mass"  : "soot_mass[ug/g]",
-        "N_agg"      : "N_agg[#/g]",
-        "d_p"        : "d_p[nm]",
-        "d_m"        : "d_m[nm]"
+        'soot_mass'  : 'soot_mass[ug/g]',
+        'N_agg'      : 'N_agg[#/g]',
+        'd_p'        : 'd_p[nm]',
+        'd_m'        : 'd_m[nm]'
     };
     for param in param_dict.keys():
         plt.rcParams.update({'font.family':'Times New Roman', 'font.size': FONTSIZE});
@@ -70,26 +70,26 @@ def plot(
 
         # Numerical
         for particle_dynamics_model_index, particle_dynamics_model in enumerate(particle_dynamics_model_types):
-            z_data = results_df[particle_dynamics_model]["z[m]"]*100;
+            z_data = results_df[particle_dynamics_model]['z[m]']*100;
             f_data = results_df[particle_dynamics_model][param_dict[param]];
             ax.plot(
                 z_data, f_data,
                 color= COLORS[particle_dynamics_model_index], 
                 linewidth = LINEWIDTH, label = LABELS[particle_dynamics_model],
-                linestyle = "solid"
+                linestyle = 'solid'
             );
             
         # Experimental
-        z_data = exp_df["z[m]"]*100;
+        z_data = exp_df['z[m]']*100;
         f_data = exp_df[param_dict[param]];
         ax.plot(
             z_data, f_data,
-            color= "#000000",
+            color= '#000000',
             markersize = MARKERSIZE,
-            marker = "s",
-            markerfacecolor = "#FFFFFF",
-            label = LABELS["Data"],
-            linestyle = "None"
+            marker = 's',
+            markerfacecolor = '#FFFFFF',
+            label = LABELS['Data'],
+            linestyle = 'None'
         );
         
         # Legend
@@ -99,20 +99,20 @@ def plot(
         ax.set_xlim([0, 65]);
         ax.xaxis.set_major_locator(MultipleLocator(10))
         ax.xaxis.set_minor_locator(MultipleLocator(2))
-        ax.set_xlabel("z [cm]", {'fontsize' : AXIS_FONTSIZE})
+        ax.set_xlabel('z [cm]', {'fontsize' : AXIS_FONTSIZE})
         
         # Y Axis
-        if param == "soot_mass":
-            ax.set_ylabel("Soot mass $\mathregular{[\mu g/g]}$", {'fontsize' : AXIS_FONTSIZE})
+        if param == 'soot_mass':
+            ax.set_ylabel(r'Soot mass $\mathregular{[\mu g/g]}$', {'fontsize' : AXIS_FONTSIZE})
             ax.set_ylim([0, 210]);
-        elif param == "N_agg":
+        elif param == 'N_agg':
             ax.set_yscale('log');
-            ax.set_ylabel("$\mathregular{N_{{agg}} [1/g]}$", {'fontsize' : AXIS_FONTSIZE})
+            ax.set_ylabel(r'$\mathregular{N_{{agg}} [1/g]}$', {'fontsize' : AXIS_FONTSIZE})
             ax.set_ylim([1e11, 3e14]);
-        elif param == "d_p":
-            ax.set_ylabel("Primary Particle Diameter $\mathregular{[nm]}$", {'fontsize' : AXIS_FONTSIZE})
+        elif param == 'd_p':
+            ax.set_ylabel(r'Primary Particle Diameter $\mathregular{[nm]}$', {'fontsize' : AXIS_FONTSIZE})
             ax.set_ylim([1, 15]);
-        elif param == "d_m":
-            ax.set_ylabel("Mobility Diameter $\mathregular{[nm]}$", {'fontsize' : AXIS_FONTSIZE})
+        elif param == 'd_m':
+            ax.set_ylabel(r'Mobility Diameter $\mathregular{[nm]}$', {'fontsize' : AXIS_FONTSIZE})
             ax.set_ylim([2, 80]);
         _save_plot(param)
--- examples/Reactors/PlugFlow/ethylene_pyrolysis/run.py
+++ examples/Reactors/PlugFlow/ethylene_pyrolysis/run.py
@@ -1,5 +1,7 @@
 from collections import namedtuple
 import pandas as pd
+import sys
+sys.path.append('../../../..')
 from simulate_flowreactor import simulate_flowreactor
 from plot import plot
 
@@ -8,27 +10,31 @@ PAH_growth_model_types = ["ReactiveDimerization",
                           "EBridgeModified", 
                           "IrreversibleDimerization"]
 
-particle_dynamics_model_types = ["Monodisperse", "Sectional"]
-df = pd.read_csv("./data/temperature_profile.csv")
-TemperatureProfile = namedtuple("TemperatureProfile",["z","T"])
-temperature_profile = TemperatureProfile(z = df["z[cm]"].to_numpy(), T = df["T[K]"].to_numpy());
+particle_dynamics_model_types = ["Monodisperse", 
+                                 "Sectional"]
 
-arg_dict = dict(
-    mech_name = "Caltech",
-    PAH_growth_model_type = PAH_growth_model_types[0],
-    particle_dynamics_model_type = particle_dynamics_model_types[0],
-    precursors = ["A2", "A3", "A4", "A2R5"],
-    inlet_composition = {"C2H4":0.01, "N2": 0.99},
-    mdot = 0.39915,
-    P = 101350,
-    temperature_profile = temperature_profile,
-    reactor_length = 0.64
-);
+df = pd.read_csv("./data/temperature_profile.csv")
+
+TemperatureProfile = namedtuple("TemperatureProfile",["z","T"])
+temperature_profile = TemperatureProfile(z = df["z[cm]"].to_numpy(), 
+                                         T = df["T[K]"].to_numpy())
+
+kwargs = {
+    'mech_name':                'Caltech',
+    'PAH_growth_model_type':    PAH_growth_model_types[0],
+    'particle_dynamics_model_type':particle_dynamics_model_types[0],
+    'precursors':               ['A2', 'A3', 'A4', 'A2R5'],
+    'inlet_composition':        {'C2H4':0.01, 'N2': 0.99},
+    'mdot':                     0.39915,
+    'P':                        101350,
+    'temperature_profile':      temperature_profile,
+    'reactor_length':           0.64,
+    }
 
 # Simulate
 for particle_dynamics_model_type in particle_dynamics_model_types:
-    arg_dict["particle_dynamics_model_type"] = particle_dynamics_model_type;
-    simulate_flowreactor(**arg_dict);
+    kwargs["particle_dynamics_model_type"] = particle_dynamics_model_type;
+    simulate_flowreactor(**kwargs);
     
 # Plot
 plot(particle_dynamics_model_types,  PAH_growth_model_types[0]);
